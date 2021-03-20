a806437b6990c4d163e55a87379ed5eb3f8f8f8e
Michelon Souza <michelon.souza@hotmail.com>
2021-03-20 13:15:18 -0300

    feat: finish challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+export interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  onSelectGenre(genreId: number): void;
+}
+
+export function SideBar({
+  genres,
+  selectedGenreId,
+  onSelectGenre,
+}: SideBarProps): JSX.Element {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onSelectGenre(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
